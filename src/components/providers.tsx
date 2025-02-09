7d3ab578039ebf131a909d3f65e5de3f487db613
Christian Alares <christian.alares@gmail.com>
2025-02-09 11:10:40 +0100

    add openpanel

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,11 +4,22 @@ import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ConvexQueryClient } from '@convex-dev/react-query'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { OpenPanelComponent } from '@openpanel/nextjs'
 
 const NEXT_PUBLIC_CONVEX_URL = process.env.NEXT_PUBLIC_CONVEX_URL
+const NEXT_PUBLIC_OPENPANEL_DASHBOARD_CLIENT_ID = process.env.NEXT_PUBLIC_OPENPANEL_DASHBOARD_CLIENT_ID
+const OPENPANEL_SECRET = process.env.OPENPANEL_SECRET
 
 if (!NEXT_PUBLIC_CONVEX_URL) {
   throw new Error('NEXT_PUBLIC_CONVEX_URL is not set')
+}
+
+if (!NEXT_PUBLIC_OPENPANEL_DASHBOARD_CLIENT_ID) {
+  throw new Error('NEXT_PUBLIC_OPENPANEL_DASHBOARD_CLIENT_ID is not set')
+}
+
+if (!OPENPANEL_SECRET) {
+  throw new Error('OPENPANEL_SECRET is not set')
 }
 
 const convex = new ConvexReactClient(NEXT_PUBLIC_CONVEX_URL)
@@ -40,4 +51,19 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
 const ThemeProvider = ({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+export const OpenPanelProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OpenPanelComponent
+        clientId={NEXT_PUBLIC_OPENPANEL_DASHBOARD_CLIENT_ID}
+        clientSecret={OPENPANEL_SECRET}
+        trackScreenViews
+        disabled={process.env.NODE_ENV !== 'production'}
+        trackOutgoingLinks
+      />
+      {children}
+    </>
+  )
 }
